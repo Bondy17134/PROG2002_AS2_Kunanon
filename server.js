606112f68e1d533f8796a5955eb011a9b71b7058
Bondy17134 <kunanon17134@gmail.com>
2026-09-15 14:36:02 +1000

    add html path in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var app = express();
 var port = 3060;
@@ -12,6 +13,22 @@ var categoryAPI = require("./controllerAPI/category-controller");
 
 app.use("/api/events", eventAPI);
 app.use("/api/categories", categoryAPI);
+
+// Serve CSS, JavaScript and images from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Website page routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/search", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "search.html"));
+});
+
+app.get("/event", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "event.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server up and running on port ${port}`);
