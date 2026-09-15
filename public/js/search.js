d6daec73e650458fc09ff5000d77d289dd4673f4
Bondy17134 <kunanon17134@gmail.com>
2026-09-15 23:01:17 +1000

    add load categories logic for categories filter dropdown menu

--- public/js/search.js
+++ public/js/search.js
@@ -6,3 +6,33 @@ var clearButton = document.getElementById("clear-filter");
 var searchResults = document.getElementById("search-results");
 var searchMessage = document.getElementById("search-message");
 
+/*
+    Retrieve categories from the API and add them 
+    to the category dropdown.
+*/
+function loadCategories() {
+    fetch("/api/catgories")
+        .then((response) => {
+            if(!response.ok){
+                throw new Error("Unable to retrieve categories.");
+            }
+            return response.json();
+        })
+        .then((categories) => {
+            categories.forEach((category) => {
+                var option = document.createElement("option");
+
+                option.value = category.category_id;
+                option.textContent = category.name;
+
+                categorySelect.appendChild(option);
+            });
+        })
+        .catch((error) => {
+            console.error("Error fetching categories:", error);
+
+            showError(
+                "Categories could not be loaded. Other filters are still available."
+            );
+        });
+}
