6c2df41f774eab1699c85246841680e460c7b3fc
Bondy17134 <kunanon17134@gmail.com>
2026-09-15 22:53:35 +1000

    search javascript  variable initialisation

---
+++ public/js/search.js
@@ -0,0 +1,8 @@
+var searchForm = document.getElementById("searchForm");
+var dateInput = document.getElementById("event-date");
+var locationInput = document.getElementById("event-location");
+var categorySelect = document.getElementById("event-category");
+var clearButton = document.getElementById("clear-filter");
+var searchResults = document.getElementById("search-results");
+var searchMessage = document.getElementById("search-message");
+
