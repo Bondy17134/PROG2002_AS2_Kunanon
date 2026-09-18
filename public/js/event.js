57421b4e6033486b4da5d85d27becf274d2bf777
Bondy17134 <kunanon17134@gmail.com>
2026-09-18 11:01:48 +1000

    variable initialisation for event,js

---
+++ public/js/event.js
@@ -0,0 +1,3 @@
+var eventStatus = document.getElementById("event-status");
+var eventDetail = document.getElementById("event-detail");
+var registerButton = document.getElementById("register-button");
