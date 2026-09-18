1d370c626d536fdcc1dbca7e4bb75d5ebf780b7b
Bondy17134 <kunanon17134@gmail.com>
2026-09-18 11:09:23 +1000

    add the logic for obtainting the event id from the url query string

--- public/js/event.js
+++ public/js/event.js
@@ -1,3 +1,25 @@
 var eventStatus = document.getElementById("event-status");
 var eventDetail = document.getElementById("event-detail");
 var registerButton = document.getElementById("register-button");
+
+/*
+    Obtain the event ID from the URL query string
+
+    Example:
+    /event?id=1
+*/
+var urlParameters = new URLSearchParams(window.location.search);
+var eventId = urlParameters.get("id");
+
+if(!eventId || isNaN(eventId)) {
+    showError("A valid event ID was not provided.");
+} else {
+    FontFaceSetLoadEvent(eventId);
+}
+
+/*
+    Retrieve the selected event from the API
+*/
+function loadEvent(id) {
+    
+}
